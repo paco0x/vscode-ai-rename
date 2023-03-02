8a8c31eb71da486ae17d85dd48bf168dcdfe9557
Paco <liaoishere@gmail.com>
2023-03-02 09:52:55 +0800

    use "gpt-3.5-turbo" model and format the code without semi-colons

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
-import * as vscode from "vscode";
-import { getRenameSuggestions } from "./openai";
+import * as vscode from "vscode"
+import { getRenameSuggestions } from "./openai"
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,34 +8,34 @@ export function activate(context: vscode.ExtensionContext) {
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
     const disposable = vscode.commands.registerCommand("ai-rename.rename", async () => {
-        const editor = vscode.window.activeTextEditor;
+        const editor = vscode.window.activeTextEditor
         if (!editor) {
-            vscode.window.showErrorMessage("AI Rename: No active editor");
-            return;
+            vscode.window.showErrorMessage("AI Rename: No active editor")
+            return
         }
-        const config = vscode.workspace.getConfiguration("aiRename");
-        const maxCharactersNum = config.get<number>("maxCharactersNum")!;
-        const maxSuggestionsNum = config.get<number>("maxSuggestionsNum")!;
-        const maxToken = config.get<number>("maxNameLength")!;
-        const temperature = config.get<number>("temperature")!;
-        const apiKey = config.get<string>("openAiApiKey");
+        const config = vscode.workspace.getConfiguration("aiRename")
+        const maxCharactersNum = config.get<number>("maxCharactersNum")!
+        const maxSuggestionsNum = config.get<number>("maxSuggestionsNum")!
+        const maxToken = config.get<number>("maxNameLength")!
+        const temperature = config.get<number>("temperature")!
+        const apiKey = config.get<string>("openAiApiKey")
         if (!apiKey) {
-            vscode.window.showErrorMessage("AI Rename: please set OpenAI API key in configuration");
-            return;
+            vscode.window.showErrorMessage("AI Rename: please set OpenAI API key in configuration")
+            return
         }
 
-        const document = editor.document;
-        const curPos = editor.selection.active;
-        const symbol = document.getText(document.getWordRangeAtPosition(curPos));
+        const document = editor.document
+        const curPos = editor.selection.active
+        const symbol = document.getText(document.getWordRangeAtPosition(curPos))
 
         if (document.getText().length > maxCharactersNum) {
             vscode.window.showErrorMessage(
-                "AI Rename: file too large, try increasing `aiRename.maxCharactersNum` in configuration"
-            );
-            return;
+                "AI Rename: file too large, try increasing `aiRename.maxCharactersNum` in configuration",
+            )
+            return
         }
 
-        let choices = new Set<string>();
+        let choices = new Set<string>()
         try {
             choices = await vscode.window.withProgress(
                 {
@@ -44,7 +44,7 @@ export function activate(context: vscode.ExtensionContext) {
                     title: "AI Rename",
                 },
                 async progress => {
-                    progress.report({ message: "Loading naming suggestions from openAI..." });
+                    progress.report({ message: "Loading naming suggestions from openAI..." })
                     return await getRenameSuggestions(
                         apiKey,
                         maxSuggestionsNum,
@@ -52,37 +52,37 @@ export function activate(context: vscode.ExtensionContext) {
                         temperature,
                         document.getText(),
                         symbol,
-                        curPos
-                    );
-                }
-            );
+                        curPos,
+                    )
+                },
+            )
         } catch (error) {
-            vscode.window.showErrorMessage(`AI Rename: request failed -> ${error}`);
-            return;
+            vscode.window.showErrorMessage(`AI Rename: request failed -> ${error}`)
+            return
         }
 
         if (choices.size === 0) {
-            vscode.window.showErrorMessage("AI Rename: failed to generate renaming choices");
-            return;
+            vscode.window.showErrorMessage("AI Rename: failed to generate renaming choices")
+            return
         }
 
-        console.log("new names: ", choices);
-        const pick = await vscode.window.showQuickPick([...choices]);
+        console.log("new names: ", choices)
+        const pick = await vscode.window.showQuickPick([...choices])
         if (!pick) {
-            console.log("AI Rename: user canceled");
-            return;
+            console.log("AI Rename: user canceled")
+            return
         }
 
         const edit = await vscode.commands.executeCommand<Promise<vscode.WorkspaceEdit>>(
             "vscode.executeDocumentRenameProvider",
             document.uri,
             curPos,
-            pick
-        );
-        await vscode.workspace.applyEdit(edit);
-    });
+            pick,
+        )
+        await vscode.workspace.applyEdit(edit)
+    })
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable)
 }
 
 // This method is called when your extension is deactivated
--- src/openai.ts
+++ src/openai.ts
@@ -1,20 +1,27 @@
-import fetch from "node-fetch";
-import { Position } from "vscode";
+import fetch from "node-fetch"
+import { Position } from "vscode"
+
+interface Message {
+    role: string
+    content: string
+    index: number
+    finish_reason: string
+}
 
 interface Choice {
-    text: string;
+    message: Message
 }
 
 interface Error {
-    code: string;
-    message: string;
-    type: string;
-    param: string | null;
+    code: string
+    message: string
+    type: string
+    param: string | null
 }
 
 interface Response {
-    error: Error;
-    choices: Array<Choice>;
+    error: Error
+    choices: Array<Choice>
 }
 
 export async function getRenameSuggestions(
@@ -24,36 +31,46 @@ export async function getRenameSuggestions(
     temperature: number,
     codeText: string,
     symbol: string,
-    pos: Position
+    pos: Position,
 ) {
-    const prompt = `I'll give a bunch of code in {[@@(CODE)@@]} format, CODE is the code text you needed. Rename the symbol at the ${pos.line} line, the ${pos.character} character in CODE, the original name is ${symbol}. The new name should follow the naming convention of the programming language, and should be a meaningful name based on the context of the code. The new name cannot be conflicted with other names in the scope of the symbol and should be different from the original name. You only need to return the new name without any other word. {[@@(${codeText})@@]}`;
+    const systemInstruction =
+        "You're a coding assistant. I'll give you a bunch of code in {[@@(CODE)@@]} format, CODE is the code you need to analyze. You'll need to give me rename suggestions of the variables or functions in the code. The new name should follow the naming convention of the programming language, and should be a meaningful name based on the context of the code. The new name cannot be conflicted with other names in the scope of the symbol and should be different from the original name."
 
     const payload = {
-        model: "text-davinci-003",
-        prompt,
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: "system",
+                content: systemInstruction,
+            },
+            {
+                role: "user",
+                content: `Here's the code: {[@@(${codeText})@@]}. Rename the symbol at the ${pos.line} line, the ${pos.character} character in CODE, the original name is ${symbol}. Only return the new name and put it in double quotes.`,
+            },
+        ],
         n,
         temperature: temperature,
         max_tokens: maxToken,
         stream: false,
-    };
-    const data = await fetch("https://api.openai.com/v1/completions", {
+    }
+    const data = await fetch("https://api.openai.com/v1/chat/completions", {
         headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${apiKey}`,
         },
         method: "POST",
         body: JSON.stringify(payload),
-    });
+    })
 
-    const response: Response = await data.json();
+    const response: Response = await data.json()
     if (response.error) {
-        throw new Error(JSON.stringify(response.error));
+        throw new Error(JSON.stringify(response.error))
     }
 
     const choices = response.choices.reduce((prev, cur) => {
-        const choice = cur.text.replace(/(\r\n|\n|\r)/gm, "");
-        return prev.add(choice);
-    }, new Set<string>());
+        const result = cur.message.content.match(/"(\w+)"/)
+        return result ? prev.add(result[1]) : prev
+    }, new Set<string>())
 
-    return choices;
+    return choices
 }
